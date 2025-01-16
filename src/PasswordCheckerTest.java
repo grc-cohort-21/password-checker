b0e5e7c7ff2343ed7c461c22707a09f61a230496
mlarsen <174657206+mlarsen-source@users.noreply.github.com>
2025-01-16 12:03:15 -0800

    created test map

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -1,14 +1,45 @@
 import static org.junit.Assert.assertEquals;
 
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
+
+  Set<String> testSet = new HashSet<>(Arrays.asList("password123", "123456", "qwerty", "letmein", "password", "hello"));
 
   @Test
   public void testDummy() {
     // This test is used only to validate that your VS Code setup is working
     // Delete this test after you have written your other tests
     assertEquals(0, 0);
+  }
+
+
+
+
+
+
+
+
+
+
+
+
+  @Test
+  public void doesBannedPasswordsreturnPassword()
+  {
+    // Arrange
+    PasswordChecker test = new PasswordChecker(10,10,testSet);
+
+    // Act
+   
+    
+    // Assert
+    
+
   }
 
 }
