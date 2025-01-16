c55eba9a5f1b7b31779c2bc66b0f2f8971070965
JSaiyan <183049245+JSaiyan@users.noreply.github.com>
2025-01-16 12:13:37 -0800

    changes to make space

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -2,13 +2,84 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class PasswordCheckerTest {
+public class PasswordCheckerTest 
+{
 
   @Test
-  public void testDummy() {
+  public void testDummy() 
+  {
     // This test is used only to validate that your VS Code setup is working
     // Delete this test after you have written your other tests
-    assertEquals(0, 0);
+    
   }
+    
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//Arrange
+
+
+    //Act
+
+    //Assert
+
+    public boolean isAlphanumeric(String password) 
+    {
+      for (int i = 0; i < password.length() - 1; i++) 
+      {
+          char c = password.charAt(i);
+          if (!Character.isLetterOrDigit(c)) 
+          {
+              return false;
+          } else 
+          {
+              return true;
+          }
+      }
+      return true;
+  }
+
+  }
+
+
+
+
+
+
 
 }
