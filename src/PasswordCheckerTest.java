f00e66160ff60537e126e58a80d35461ab0e8616
xavierb <194048107+xavierb117@users.noreply.github.com>
2025-01-16 11:48:14 -0800

    Added one test to describePasswordLength

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -3,12 +3,13 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
-
   @Test
-  public void testDummy() {
-    // This test is used only to validate that your VS Code setup is working
-    // Delete this test after you have written your other tests
-    assertEquals(0, 0);
+  public void desribePasswordLengthVerification() {
+    // Arrange
+    PasswordChecker password = new PasswordChecker(6, 12);
+    // Act
+    String hi = password.describePasswordLength("hi");
+    // Assert
+    assertEquals("short", hi);
   }
-
 }
