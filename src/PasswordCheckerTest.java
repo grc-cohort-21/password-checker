163f7ffa02382928ee944a773de1ebba503a06c8
Shawn Nguru <nguru.shawn@student.greenriver.edu>
2025-01-16 14:21:37 -0600

    please work

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -6,20 +6,18 @@ public class PasswordCheckerTest
 {
 
   @Test
-  public void testingDescribePasswordLengthShort() {
+  public void testingDescribePasswordLengthShort() 
+  {
   PasswordChecker checker = new PasswordChecker(5, 8);
    String pass = "1234";
     assertEquals(checker.describePasswordLength(pass), "short");
-    assertEquals(checker.describePasswordLength("asaaaaa"), "medium");
   }
-
-
-  @Test
-  public void testingDescribePasswordLengthLong() {
-  PasswordChecker checker = new PasswordChecker(1, 4);
-   String pass = "asdfasdf";
-    assertEquals(checker.describePasswordLength(pass), "long");
-    
+  
+  public void testingDescribePasswordLengthMedium() 
+  {
+  PasswordChecker checker = new PasswordChecker(5, 8);
+   String pass = "12345678";
+    assertEquals(checker.describePasswordLength(pass), "medium");
   }
 
 
