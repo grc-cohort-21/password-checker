2d53e442f91e8eb22c5ca479006a603cf700cdc9
Maple <135763163+maple-johnson@users.noreply.github.com>
2025-01-16 11:59:05 -0800

    Added short-length test for password length

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -2,13 +2,19 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-public class PasswordCheckerTest {
+public class PasswordCheckerTest 
+{
 
   @Test
-  public void testDummy() {
-    // This test is used only to validate that your VS Code setup is working
-    // Delete this test after you have written your other tests
-    assertEquals(0, 0);
+  public void testPasswordLengthShort()
+  {
+     // Arrange
+     PasswordChecker shortName = new PasswordChecker(6, 12);
+     String password = "Green";
+     // Act
+     String actualLength = shortName.describePasswordLength(password);
+     // Assert
+     assertEquals("short", actualLength);
   }
 
 }
