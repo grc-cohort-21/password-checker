1cabf5a72332dc80ba563904138820bf3e82e69c
Jacob Woodbury <166659344+JacobWoodbury@users.noreply.github.com>
2025-01-16 12:24:33 -0800

    troubleshooting

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -6,18 +6,26 @@ public class PasswordCheckerTest
 {
 
   @Test
-  public void testingDescribePasswordLengthShort() 
-  {
+  public void testingDescribePasswordLengthShort() {
   PasswordChecker checker = new PasswordChecker(5, 8);
    String pass = "1234";
     assertEquals(checker.describePasswordLength(pass), "short");
+    assertEquals(checker.describePasswordLength("asaaaaa"), "medium");
   }
-  
+
   public void testingDescribePasswordLengthMedium() 
   {
   PasswordChecker checker = new PasswordChecker(5, 8);
    String pass = "12345678";
     assertEquals(checker.describePasswordLength(pass), "medium");
+  }
+
+  @Test
+  public void testingDescribePasswordLengthLong() {
+  PasswordChecker checker = new PasswordChecker(1, 4);
+   String pass = "asdfasdf";
+    assertEquals(checker.describePasswordLength(pass), "long");
+    
   }
 
 
