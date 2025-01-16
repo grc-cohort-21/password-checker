8d1a1bca422951a00fdb331658336c5eb5f91d7b
Auberon Lopez <auberon.lopez@greenriver.edu>
2025-01-16 08:17:35 -0800

    Added dummy test

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -4,4 +4,11 @@ import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
 
+  @Test
+  public void testDummy() {
+    // This test is used only to validate that your VS Code setup is working
+    // Delete this test after you have written your other tests
+    assertEquals(0, 0);
+  }
+
 }
