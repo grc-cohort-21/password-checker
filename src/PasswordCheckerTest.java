80229756968f5c323b5c6c42b7100da2f252dc7a
Jacob Woodbury <166659344+JacobWoodbury@users.noreply.github.com>
2025-01-16 12:16:18 -0800

    3rd test

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -5,11 +5,20 @@ import org.junit.jupiter.api.Test;
 public class PasswordCheckerTest {
 
   @Test
-  public void testingDescribePasswordLength() {
+  public void testingDescribePasswordLengthShort() {
   PasswordChecker checker = new PasswordChecker(5, 8);
    String pass = "1234";
     assertEquals(checker.describePasswordLength(pass), "short");
-    assertEquals(checker.describePasswordLength("asfdasd"), "medium");
+    assertEquals(checker.describePasswordLength("asaaaaa"), "medium");
+  }
+
+
+  @Test
+  public void testingDescribePasswordLengthLong() {
+  PasswordChecker checker = new PasswordChecker(1, 4);
+   String pass = "asdfasdf";
+    assertEquals(checker.describePasswordLength(pass), "long");
+    
   }
 
 }
