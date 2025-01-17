832cb92ef9a5c8be940e0ca0fb143cbcb6ee8a65
mlarsen <174657206+mlarsen-source@users.noreply.github.com>
2025-01-16 23:10:59 -0800

    added JUnit tests for isAlphanumeric method

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -46,112 +46,71 @@ public class PasswordCheckerTest
   
   
   
+    
+  ////  isBannedPassword tests  ////
+    
   
   // tests if isBannedPassword method returns true if a default banned password is entered
-    @Test
-    public void doesIsBannedPasswordReturnTrueWhenDefaultInvalid()
-    {
-      // Arrange
-      PasswordChecker testCheck = new PasswordChecker(10,10,customSet);
-  
-      // Act
-      boolean result = testCheck.isBannedPassword("password");
-           
-      // Assert
-      assertEquals(true, result);  
-    }
+  @Test
+  public void doesIsBannedPasswordReturnTrueWhenDefaultInvalid()
+  {
+    PasswordChecker testChecker = new PasswordChecker(6,12,customSet);
+    boolean result = testChecker.isBannedPassword("password");   
+    assertEquals(true, result);  
+  }
 
 
-     // tests if isBannedPassword method returns true if a custom banned password is entered
-     @Test
-     public void doesIsBannedPasswordReturnTrueWhenCustomInvalid()
-     {
-       // Arrange
-       PasswordChecker testCheck = new PasswordChecker(10,10,customSet);
-   
-       // Act
-       boolean result = testCheck.isBannedPassword("opensesame");
-            
-       // Assert
-       assertEquals(true, result);  
-     }
-
-    
-    // tests if isBannedPassword method returns false if a valid password is entered
-    @Test
-    public void doesIsBannedPasswordReturnFalseWhenValid()
-    {
-      // Arrange
-      PasswordChecker testCheck = new PasswordChecker(10,10,customSet);
-  
-      // Act
-      boolean result = testCheck.isBannedPassword("thisaValidpassword123");
-           
-      // Assert
-      assertEquals(false, result);  
-    }
-  
+  // tests if isBannedPassword method returns true if a custom banned password is entered
+  @Test
+  public void doesIsBannedPasswordReturnTrueWhenCustomInvalid()
+  {
+    PasswordChecker testChecker = new PasswordChecker(6,12,customSet);
+    boolean result = testChecker.isBannedPassword("opensesame");    
+    assertEquals(true, result);  
+  }
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//Arrange
-
-
-    //Act
-
-    //Assert
-
-    public boolean isAlphanumeric(String password) 
-    {
-      for (int i = 0; i < password.length() - 1; i++) 
-      {
-          char c = password.charAt(i);
-          if (!Character.isLetterOrDigit(c)) 
-          {
-              return false;
-          } else 
-          {
-              return true;
-          }
-      }
-      return true;
-    
+  // tests if isBannedPassword method returns false if a valid password is entered
+  @Test
+  public void doesIsBannedPasswordReturnFalseWhenValid()
+  {
+    PasswordChecker testChecker = new PasswordChecker(6,12,customSet);
+    boolean result = testChecker.isBannedPassword("thisaValidpassword123");  
+    assertEquals(false, result);  
   }
 
 
 
+  ////  isAlphaNumeric Tests  ////
 
 
-
-
-
-
-
-
-
-
- 
-
+  // tests if isAlphaNumeric method returns true if a valid password is entered
+  @Test
+  public void doesIsAlphaNumericReturnTrueWhenValid()
+  {
+    PasswordChecker testChecker = new PasswordChecker(6,12,customSet);
+    boolean result = testChecker.isAlphanumeric("abc12345");
+    assertEquals(true, result);  
   }
+
+  // tests if isAlphaNumeric method returns false if special characters are used in password
+  @Test
+  public void doesIsAlphaNumericReturnFalseWhenInvalidSpecial()
+  {
+    PasswordChecker testChecker = new PasswordChecker(6,12,customSet);
+    boolean result = testChecker.isAlphanumeric("@bc%123$!");
+    assertEquals(false, result);  
+  }
+
+  // tests if isAlphaNumeric method returns false if a spaces are used in password
+  @Test
+  public void doesIsAlphaNumericReturnFalseWhenInvalidSpaces()
+  {
+    PasswordChecker testChecker = new PasswordChecker(6,12,customSet);
+    boolean result = testChecker.isAlphanumeric(" abc 12 34 5");
+    assertEquals(false, result);  
+  }
+
+
+  
+ }
