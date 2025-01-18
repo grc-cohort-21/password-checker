91c785baa1dd75edc14ea181a9299d420a74229f
AAshGray <193518051+AAshGray@users.noreply.github.com>
2025-01-17 19:00:37 -0800

    removed testDummy

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -3,14 +3,7 @@ import static org.junit.Assert.assertEquals;
 import org.junit.jupiter.api.Test;
 
 public class PasswordCheckerTest {
-
-  @Test
-  public void testDummy() {
-    // This test is used only to validate that your VS Code setup is working
-    // Delete this test after you have written your other tests
-    assertEquals(0, 0);
-  }
-
+  
   //3 tests for describePasswordLength
   @Test
   public void testShortPasswordLength() {
