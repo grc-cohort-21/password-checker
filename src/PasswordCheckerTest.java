bd120a612bc8bc9705dda7d9d5572699c31a334b
RJ <fredrickalexander1727@gmail.com>
2025-01-21 22:15:11 -0800

    01.21.2025 10:14pm commit, 9/9 testcases

--- src/PasswordCheckerTest.java
+++ src/PasswordCheckerTest.java
@@ -1,5 +1,4 @@
 import static org.junit.Assert.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
@@ -30,51 +29,48 @@ public class PasswordCheckerTest {
     PasswordChecker check = new PasswordChecker(5, 10);
     String password = "a";
     String actualLength = check.describePasswordLength(password);
-    assertEquals("Expected: short","short",actualLength);
+    assertEquals("Expected: Short","short",actualLength);
   }
-
-  // @Test public void testFive() {
-  //   // Arrange
-
-  //   // Act
-
-  //   // Assert
+  @Test
+  public void testifTooLong() {
+    PasswordChecker check = new PasswordChecker(5, 10);
+    String password = "fdjfhdjsfhsdfjksdfkjsdfjsdh";
+    String actualLength = check.describePasswordLength(password);
+    assertEquals("Expected: Long","long",actualLength);
+  }
+  @Test
+  public void testifreturnasMedium() {
+    PasswordChecker check = new PasswordChecker(5, 10);
+    String password = "fdjfhdf";
+    String actualLength = check.describePasswordLength(password);
+    assertEquals("Expected: Medium","medium",actualLength);
+  }
+  @Test
+  public void testifalphanumericwhenmixed() { // test 7
+    // Arrange - all the stuff you need before starting your test
+    PasswordChecker check = new PasswordChecker(5, 10);
+    boolean actualTF = check.isAlphanumeric("dsajhk5435");
+    // Act - actually making something happen; calling a method
+    assertEquals("Expected: True",true,actualTF);
+    // Assert - did what i actually got match what i was expecting
+  }
+  @Test
+  public void testifalphamericwhenallnumbers() {
+    // Arrange
+    PasswordChecker check = new PasswordChecker(5, 10);
+    boolean actualTF = check.isAlphanumeric("dsajhkfdfds");
+    // Act
+    assertEquals("Expected: True",true,actualTF);
+    // Assert
+  }
+  @Test
+  public void testifalphamericwhenallnumberswithspecialstuff() {
+    // Arrange
+    PasswordChecker check = new PasswordChecker(5, 10);
+    boolean actualTF = check.isAlphanumeric("$#@@#dsajh%$#%#!!!$kfdfds");
+    // Act
+    assertEquals("Expected: False",false,actualTF);
+    // Assert
     
-  // }
-
-  // @Test public void testSix() {
-  //   // Arrange
-
-  //   // Act
-
-  //   // Assert
-    
-  // }
-
-  // @Test public void testIfAllUpperCaseBannedPasswordsWork() { // test 7
-  //   // Arrange - all the stuff you need before starting your test
-  //   isBannedPassword uppercaseHello = new isBannedPassword("HELLO");
-
-  //   // Act - actually making something happen; calling a method
-
-  //   // Assert - did what i actually got match what i was expecting
-  // }
-
-  // @Test public void testEight() {
-  //   // Arrange
-
-  //   // Act
-
-  //   // Assert
-    
-  // }
-
-  // @Test public void testNine() {
-  //   // Arrange
-
-  //   // Act
-
-  //   // Assert
-    
-  // }
+  }
 }
